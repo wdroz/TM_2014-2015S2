7a437b10584536df6d9d1b4004c68fb9f9a7a5ea
wdroz <william.droz.ch@gmail.com>
2015-03-11 15:10:31 +0100

    data can be writted using pickle

--- src/DataWritter.py
+++ src/DataWritter.py
@@ -15,10 +15,11 @@ class DataWritter(object):
         self.cpt = 0
         
     def serializeOne(self, item):
-        fullpath = os.path.join(self.path, '%s-%d.p' % (time.strftime('%Y-%m-%d_%H:%M:%S'), self.cpt))
-        with open(fullpath, 'w') as f: 
-            self.serializer(item.__dict__, f)
+        fullpath = os.path.join(self.path, '%s-%d.p' % (time.strftime('%Y-%m-%d_%H_%M_%S'), self.cpt))
+        with open(fullpath, 'w') as outfile:
+            self.serializer(item.__dict__, outfile)
         self.cpt += 1
+        
     
     def serialize(self, collection):
         for item in collection:
