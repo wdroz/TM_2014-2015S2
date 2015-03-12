b06efccf22ed8f37f893d34cbcb0ac796cbddc61
wdroz <william.droz.ch@gmail.com>
2015-03-12 13:19:22 +0100

    extract some features

---
+++ src/FeaturesManager.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 15:46:45 2015
+
+@author: droz
+"""
+
+from textblob import TextBlob
+import re
+
+class Features(object):
+    def __init__(self, news):
+        self.news = news
+        self.publication = re.sub(r'[^A-Za-z1234567890.,$!'']', '', news.publication)    
+        #TODO remove special chars
+        self.textBlob = TextBlob(self.publication)
+        self.polarity = self.textBlob.sentiment.polarity
+        self.processMarketStatus()
+    
+    def processMarketStatus(self):
+        self.marketChange = []
+        self.marketChangeEndToEnd = 0
+        for marketStatus in self.news.marketStatus:
+            deltaMarket = marketStatus.market_close-marketStatus.market_open
+            self.marketChange.append(float(deltaMarket)/marketStatus.market_open)     
+            
+        try:
+            deltaMarket = self.news.marketStatus[-1].market_close-self.news.marketStatus[0].market_open
+            self.marketChangeEndToEnd = float(deltaMarket)/self.news.marketStatus[0].market_open
+        except:
+            pass # empty
+            
+    def isGood(self):
+        return self.marketChangeEndToEnd > 0.0
+        
+    def __str__(self):
+        return str(self.getVector()) + '\t -> ' + ('good' if self.isGood() else 'bad')
+        
+    def getVector(self):
+        return [self.polarity, self.marketChangeEndToEnd] + self.marketChange
+
+class FeaturesManager(object):
+    def __init__(self):
+        pass
+        
--- src/SparkMain.py
+++ src/SparkMain.py
@@ -10,13 +10,28 @@ from os import listdir
 from os.path import isfile, join
 
 from pyspark import SparkContext
+from FeaturesManager import Features
+from DataManager import News
 
 def decode(x):
-    return pickle.load(open(x, 'r'))
+    news = News()
+    news.__dict__ = pickle.load(open(x, 'r'))
+    return news
+    
 if __name__ == "__main__":
     path = '/media/droz/KIKOOLOL HDD/Corpus/pickle/'
     sc = SparkContext()
     onlyfiles = [ join(path,f) for f in listdir(path) if isfile(join(path,f)) ]
     newsRDD = sc.parallelize(onlyfiles).map(decode)
-    for new in newsRDD.collect():
-        print(str(new))
+    FeaturesRdd = newsRDD.map(lambda x: Features(x))
+    
+    nbGood = 0
+    nbBad = 0
+    filteredFeaturesRDD = FeaturesRdd.filter(lambda x: len(x.getVector()) == 5)
+    for feature in  filteredFeaturesRDD.collect():
+        print(feature)
+        if(feature.isGood()):
+            nbGood += 1
+        else:
+            nbBad += 1
+    print("nb good : %d\nnb bad %d" % (nbGood, nbBad))
