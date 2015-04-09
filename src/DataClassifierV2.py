eaaeed459cbc8bfc1677cb5545ff371178b2c41e
wdroz <william.droz.ch@gmail.com>
2015-04-09 17:26:50 +0200

    Add specific classifier that contain other classifiers

---
+++ src/DataClassifierV2.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  9 14:57:58 2015
+
+@author: droz
+"""
+
+class DataClassifierV2(object):
+        pass
+    
+
+class ClassifiersWrapper(object):
+    '''
+    Classe for use multiple classifier like one
+    '''
+    def __init__(self):
+        self.classifiers = []
+        self.models = []
+    
+    def addClassifier(self, **kwargs):
+        try:
+            classifier = kwargs['classifier']
+            trainParameters = kwargs['trainParameters']
+            weight = kwargs['weight']
+            self.classifiers.append((classifier, trainParameters, weight))
+        except:
+            pass
+        
+    def train(self, dataset):
+        for (classifier, trainParameters, weight) in self.classifiers:
+            model = classifier.train(dataset, **trainParameters)
+            self.models.append((model, weight))
+            
+        return self
+        
+    def predict(self, vect):
+        predictions = {}     
+        bestPred = 0
+        bestPredWeight = 0
+        for (model, weight) in self.models:
+            res = model.predict(vect)
+            if res not in predictions:
+                predictions[str(res)] = 0
+            predictions[str(res)] += weight
+            if(predictions[str(res)] > bestPredWeight):
+                bestPredWeight = predictions[str(res)]
+                bestPred = res
+        return bestPred
+            
+        
