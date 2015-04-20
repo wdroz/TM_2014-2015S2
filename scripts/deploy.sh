18e11f836b6a6bc15a9788b41cf35d6cf1a35773
wdroz <william.droz.ch@gmail.com>
2015-04-20 15:47:09 +0200

    deploy on spark 1.3.1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ server=157.26.83.52
 tmpZipList=zipList.txt
 ./createZips.sh
 scp *.zip $main $user@$server:~
+scp ../src/*.p $main $user@$server:~
 ls -1 *.zip > $tmpZipList
 zipString=""
 while read line
@@ -12,6 +13,6 @@ do
    zipString="$line,$zipString"
 done < $tmpZipList
 rm $tmpZipList
-command=$(echo "./spark-1.2.1-bin-hadoop2.4/bin/spark-submit --deploy-mode client --master yarn-client --py-files \"$zipString\" $main")
+command=$(echo "./spark-1.3.1-bin-hadoop2.4/bin/spark-submit --deploy-mode client --master yarn-client --py-files \"$zipString\" --archives \"nltk_data.zip\" $main")
 #ssh $user@$server ''$command''
 ./runOnServer $user $server "$command"
