e3dcbbfba373e5e1a03a2b89a353bbcce7863457
wdroz <william.droz.ch@gmail.com>
2015-04-20 14:37:51 +0200

    Can now using one vs one or one vs many

--- src/DataClassifierV2.py
+++ src/DataClassifierV2.py
@@ -5,9 +5,62 @@ Created on Thu Apr  9 14:57:58 2015
 @author: droz
 """
 from pyspark.mllib.regression import LabeledPoint
-from copy import deepcopy
 
-class DataClassifierMultiClasses(object):
+class DataClassifierMultiClassesOneVsMany(object):
+    '''
+    Classe qui permet de classifier avec du multiclasse
+    '''
+    def __init__(self, binaryClassifier, numClasses):
+        self.binaryClassifier = binaryClassifier
+        self.numClasses = numClasses
+        self.models = []
+        
+    def _giveOneAndMany(self, rdds, nb):
+        middle = rdds[nb] 
+        leftPart = rdds[0:nb]
+        rightPart = rdds[nb+1:]
+        left = None
+        right = None
+        if(leftPart != []):
+            left = reduce(lambda x,y : x.union(y), leftPart)
+        if(rightPart != []):
+            right = reduce(lambda x,y : x.union(y), rightPart)
+            
+        if(left == None):
+            return middle, right
+        if(right == None):
+            return middle, left
+        
+        return middle, left.union(right)
+        
+    def train(self, dataset):
+        datasetOnlyX = []
+        # Create a dataset per classes
+        for i in range(self.numClasses):
+            specRdd = dataset.filter(lambda x: x.label == i)
+            specRdd.cache()
+            datasetOnlyX.append(specRdd)
+        self.models = []
+        for i in range(self.numClasses):
+            one, many = self._giveOneAndMany(datasetOnlyX, i)    
+            aRdd = one.map(lambda x: LabeledPoint(0, x.features))
+            bRdd = many.map(lambda x: LabeledPoint(1, x.features))
+            print('\tsize a : %d, size b : %s' % (aRdd.count(), bRdd.count()))
+            model = self.binaryClassifier.train(aRdd.union(bRdd))
+            self.models.append(model)
+        return self
+            
+    
+    def predict(self, vect):
+        for index in range(self.numClasses):
+            #print('predict - index %d' % index)
+            model = self.models[index]
+            pred = model.predict(vect)
+            if(pred == 0):
+                return index
+        return 0 # doesn't know
+
+class DataClassifierMultiClassesOneVsOne(object):
     '''
     Classe qui permet de classifier avec du multiclasse
     '''
