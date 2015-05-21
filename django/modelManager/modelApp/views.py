b7d97066b26b939dc25cfe640795dd0801353c2f
wdroz <william.droz.ch@gmail.com>
2015-05-21 10:22:20 +0200

    use templates for show full model

--- django/modelManager/modelApp/views.py
+++ django/modelManager/modelApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from modelApp.models import PredictModel
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+    
+def showModels(request):
+    myPredictModels = PredictModel.objects.all()
+    return render(request, 'showmodels.html', {'models' : myPredictModels})
