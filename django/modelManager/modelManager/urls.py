995a1b95a85247ef0b0a1d344a75ad396980aeda
wdroz <william.droz.ch@gmail.com>
2015-05-21 09:41:46 +0200

    update url for better match

--- django/modelManager/modelManager/urls.py
+++ django/modelManager/modelManager/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from modelApp import views
 
 urlpatterns = [
-    url(r'', views.index),
+    url(r'^$', views.index),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^showmodels/', views.showModels),
 ]
