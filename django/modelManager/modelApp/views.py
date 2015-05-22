44201e6364e1f2cf01ccf9d8a0cac89c1d7157a6
wdroz <william.droz.ch@gmail.com>
2015-05-22 13:50:13 +0200

    create other kind of chart for prediction

--- django/modelManager/modelApp/views.py
+++ django/modelManager/modelApp/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 from django.utils import timezone
 
 import pygal
-from datetime import datetime
 from datetime import timedelta
-
+from tools_django import matrix_normalize
+from tools_django import predictPointToList
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
@@ -15,29 +15,9 @@ def index(request):
 def showModels(request):
     myPredictModels = PredictModel.objects.all()
     return render(request, 'showmodels.html', {'models' : myPredictModels})
-    
-def predictPointToList(predictPoints, hours=48, numberOfX=12):
-    #end = datetime.now()
-    end = timezone.now()    
-    start = end - timedelta(hours=hours)
-    nbInLine = []
-    for i in range(numberOfX):
-        nbInLine.append(0)
-    # interpolate and make bins
-    dx = hours/numberOfX
-    for p in predictPoints:
-        if(p.newsPubDate <= end and p.newsPubDate >= start):
-            for i in range(numberOfX):
-                left = start + timedelta(hours=dx*i)
-                right = left + timedelta(hours=dx)
-                if(p.newsPubDate >= left and p.newsPubDate <= right):
-                    nbInLine[i] += 1
-                    break
-    return nbInLine
-    
-    
-def graph_stacked(request, graph_name):
-    hours = 48
+        
+def graph(request, graph_name):
+    hours = 36
     numberOfX = 4
     
     graph = PredictGraph.objects.get(name=graph_name)
@@ -51,40 +31,20 @@ def graph_stacked(request, graph_name):
     for i in range(numberOfX):
         labels.append(str(start + timedelta(hours=dx*i)))
     stackedline_chart = pygal.StackedLine(fill=True, x_label_rotation=30)
-    stackedline_chart.title = 'market prediction (in %)'
+    stackedline_chart.title = 'market prediction (in %) [' + str(graph_name) + ']'
     stackedline_chart.x_labels = labels
     veryBad = predictPointToList(graph.predictpoint_set.filter(predictScore=0.0), hours=hours, numberOfX=numberOfX)
     bad = predictPointToList(graph.predictpoint_set.filter(predictScore=1.0), hours=hours, numberOfX=numberOfX)
     good = predictPointToList(graph.predictpoint_set.filter(predictScore=2.0), hours=hours, numberOfX=numberOfX)
     veryGood = predictPointToList(graph.predictpoint_set.filter(predictScore=3.0), hours=hours, numberOfX=numberOfX)
-    
+    matrix_normalize([veryBad, bad, good, veryGood])
+
     stackedline_chart.add('Very bad', veryBad)
     stackedline_chart.add('Bad',  bad)
     stackedline_chart.add('Good',      good)
     stackedline_chart.add('Very good',  veryGood)
     return HttpResponse(stackedline_chart.render()) 
     #stackedline_chart.render()
-    
-def graph(request, graph_name):
-    return graph_stacked(request, graph_name)
-    graph = PredictGraph.objects.get(name=graph_name)
-    datey = pygal.DateY(x_label_rotation=20, pretty_print=True)
-    pList = []
-    for p in graph.predictpoint_set.all():
-        myDate = p.newsPubDate
-        pList.append((myDate, p.predictScore))
-    
-    now = datetime.now()
-    x_list = [now - timedelta(hours=48), now]
-    #for i in range(len(pList)):
-    #    x_list.append(now + timedelta(hours=-1*i))
-    
-    datey.add(str(graph_name), pList)
-    datey.x_labels = x_list
-    #datey.x_label_format = "%Y-%m-%d"
-    #datey.x_label_format = "%Y-%m-%d"
-    #datey.render_to_png('lol.png')
-    return HttpResponse(datey.render()) # image/svg+xml
     
 def prediction(request, model_name):
     if(model_name == None):
